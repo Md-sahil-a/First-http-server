92bd25d447769a529917b4f9e3dcb9ee0687922e
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-08-23 22:17:10 +0530

    endpoints

--- http-server.js
+++ http-server.js
@@ -2,14 +2,30 @@ const http = require('http');
 
 const PORT = 3000;
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({
-        'id':1,
-        'name':"MSA",
-        'role':'Programmer'
-    }));
+    if(req.url =="/data"){
+        // res.writeHead(200,{
+        //     'Content-Type': 'application/json'
+        // });
+        res.setHeader('Content-Type','application/json');
+        res.statusCode = 200;
+        res.end(JSON.stringify({
+            'id':1,
+            'name':"MSA",
+            'role':'Programmer'
+        }));
+    }else if(req.url === "/friend"){
+        res.setHeader =('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<h1> "Routing in server" </h1>');
+        res.write('</body>');
+        res.write('</html');
+        res.end();
+    }else{
+        res.statusCode = 404;
+        res.end();
+    }
+   
 });
 
 server.listen(PORT, ()=>{
